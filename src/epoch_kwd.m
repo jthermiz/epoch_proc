b7553d9cc445f7213b8cda44df70833ddef3d16c
John <jthermiz@gmail.com>
2019-03-24 15:24:27 -0700

    Corrected and add more comments to epoch_kwd

--- src/epoch_kwd.m
+++ src/epoch_kwd.m
@@ -1,19 +1,21 @@
 function Tr = epoch_kwd(kwdfile,stim,cfg)
 %Example:
     % kwdfile = 'experiment.kwd';
-    % kwefile = 'experiment.sng'kwe';
+    % stim.atime = 1 x n array of trial onset indices
+    % stim.type = 1 x n array of trial labels
+    % stim.typename = 1 x q array of trial label names (matches type #)
     % cfg.range  = [-2 2];
     % cfg.rhd_convert = 0;
-    % Tr = epoch_kwd(kwdfile, cfg);
+    % Tr = epoch_kwd(kwdfile, stim, cfg);
 
 %Note for some reason h5read is super slow with stride > 1, so ignoring
 %eliminating this an input
 
 %raw recording metadata
 %raw recording metadata
-info = h5info(kwdfile,'/recordings/0');
-chtot = info.Datasets(1).Dataspace.Size(1);
-fs = info.Attributes(2).Value(1); 
+info = h5info(kwdfile,'/recordings/0'); %recording file meta data
+chtot = info.Datasets(1).Dataspace.Size(1); %number of channels
+fs = info.Attributes(2).Value(1);  %sample rate
 convfactor = 0.195; %hardcorded since it never changes
 
 % if cfg.rhd_convert -- DELETE SOON
@@ -48,21 +50,21 @@ Tr.atime = zeros(trnum,1);
 
 
 % main Tr loop
-z = 1;
-for i=1:stim_num
+z = 1; %index of trial for Tr.data
+for i=1:stim_num %loop through all trial types
     idx = stim.type == i;
     ev = stim.atime(idx);
-    for j=1:numel(ev)
-        tic
+    for j=1:numel(ev) %loop through each trial of type 'i'
+        tic %used to track time elapsed per loop -- can be commented
         a = ev(j) + cfg.range(1)*fs;        
-        startindices = [1, a];        
-        countindices = [chtot, T];
-        dat = h5read(kwdfile,'/recordings/0/data',startindices,countindices);        
-        Tr.data(z,:,:) = single(dat')*convfactor;
-        Tr.type(z) = i;
-        Tr.atime(z) = ev(j);
-        z = z + 1;
-        toc
+        startindices = [1, a]; %get data from channel 1 starting at timepoint 'a'        
+        countindices = [chtot, T]; %through channel 'chtot' (all channels) to timepoint 'T' (period of each trial)
+        dat = h5read(kwdfile,'/recordings/0/data',startindices,countindices); %low level read function for kwd file
+        Tr.data(z,:,:) = single(dat')*convfactor; %convert to microvolts
+        Tr.type(z) = i; %trial label
+        Tr.atime(z) = ev(j); %index of trial onset
+        z = z + 1; %increment trial index
+        toc %used to track time elapsed per loop -- can be commented
     end
 end
 
